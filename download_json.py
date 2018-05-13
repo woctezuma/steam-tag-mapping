f475634a8ba3d3adcfa99dc2da46ec819418700d
Wok <woctezuma@users.noreply.github.com>
2018-05-13 17:11:44 +0200

    Separate main() functions

--- download_json.py
+++ download_json.py
@@ -1,9 +1,10 @@
 # Objective: download and cache data from SteamSpy
 
-import urllib.request, json
+import json
+import urllib.request
 
-def downloadSteamSpyData(json_filename = "steamspy.json"):
 
+def downloadSteamSpyData(json_filename="steamspy.json"):
     # If json_filename is missing, we will attempt to download and cache it from steamspy_url:
     steamspy_url = "http://steamspy.com/api.php?request=all"
 
@@ -12,10 +13,12 @@ def downloadSteamSpyData(json_filename = "steamspy.json"):
             data = json.load(in_json_file)
     except FileNotFoundError:
         print("Downloading and caching data from SteamSpy")
+
         # Trick to download the JSON file directly from SteamSpy, in case the file does not exist locally yet
         # Reference: https://stackoverflow.com/a/31758803/
         class AppURLopener(urllib.request.FancyURLopener):
             version = "Mozilla/5.0"
+
         opener = AppURLopener()
         with opener.open(steamspy_url) as response:
             data = json.load(response)
@@ -28,5 +31,12 @@ def downloadSteamSpyData(json_filename = "steamspy.json"):
 
     return data
 
-if __name__ == "__main__":
+
+def main():
     data = downloadSteamSpyData()
+
+    return True
+
+
+if __name__ == "__main__":
+    main()
